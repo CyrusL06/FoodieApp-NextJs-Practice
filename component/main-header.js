4e6e6f42ccaeed9bf7d9220bb049132ec155a06d
CyrusLorenzo <120500547+CyrusL06@users.noreply.github.com>
2024-10-01 05:23:11 +0000

    Styling NextJS using CSS Modules....

--- component/main-header.js
+++ component/main-header.js
@@ -1,18 +1,21 @@
 import Link from "next/link";
 // @ = root directory
 import logoImg from "@/assets/logo.png";
+//special way of importing CSS but u gonna use tailwind
+import classes from "./main-header.module.css";
 
 export default function MainHeader() {
   return (
-    <header>
+    <header className={classes.header}>
       {/* We need a clickable Logo to take back in home*/}
-      <Link href="/">
+      {/* we access this classes👇 object  */}
+      <Link className={classes.logo} href="/">
         {/* In next u have to pass it as ".src" */}
         <img src={logoImg.src} alt="A plate with food on it" />
         NextLevel Food
       </Link>
       {/* navigation elemnt */}
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
