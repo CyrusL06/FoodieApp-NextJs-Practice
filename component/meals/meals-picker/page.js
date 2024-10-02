3f3b934cc3c13da612847a1d38c57961babcc33e
Cyrus Lorenzo <cyruslorenzo@Cyruss-MacBook-Pro.local>
2024-10-02 13:48:22 -0700

    Handling Image Preview and Error function when its not rendered

--- component/meals/meals-picker/page.js
+++ component/meals/meals-picker/page.js
@@ -2,22 +2,49 @@
 
 
 'use client'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 // Because we use onClick
 import styles from './meals-picker.module.css'
+import Image from 'next/image';
 
 export default function ImagePicker ({label, name}){
     // trigger
     const imageInput = useRef();
+    const [pickedImage, setPickedImage] = useState();
 
     function handleClick (){
         imageInput.current.click();
+    }
+
+    function handleImageChange(event){
+        // One file that can be access
+        const file = event.target.files[0];
+
+        if(!file){
+            setPickedImage(null);
+            return;
+        }
+
+        // To preview / read our file 
+        const fileReader = new FileReader();
+
+        // generated url
+        fileReader.onload = () => {
+           setPickedImage(fileReader.result);
+        };
+
+        // pass that data in that method
+        fileReader.readAsDataURL(file);
     }
 
   return (
     <div className={styles.picker}>
         <label htmlFor={name}>{label}</label>
         <div className={styles.controls}>
+            <div className={styles.preview}>
+                {!pickedImage && <p>No Image picked yet</p>}
+                {pickedImage && (<Image src={pickedImage} alt="Image selected by user" fill/>)}
+            </div>
             {/* We want to trigger this when click which ref can do */}
              {/* specified file accepted */}
             <input 
@@ -28,6 +55,9 @@ export default function ImagePicker ({label, name}){
             accept="image/png, image/jpeg" 
             name={name}
             ref={imageInput}
+            onChange={handleImageChange}
+            // so file cannot be submitted without Image
+            required
             />
 
             {/* We want to use input button */}
